159f2bd501db0c1ee96e69c83f7cb2dc9ec0788e
LucianoEsquivel <lucianostivy@gmail.com>
2026-04-16 17:14:54 -0300

    v1.0.1.7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dietrich-cache-v1.0.1.6'; // Subimos a 1.0.1.6 para testear (16/04/2026 17:07 PM)
+const CACHE_NAME = 'dietrich-cache-v1.0.1.7'; // Subimos a 1.0.1.7 para testear (16/04/2026 17:07 PM)
 const assets = [
   './',
   './index.html',
