fc4666970efbb5369fde3c8960bbf739b61f1bd8
LucianoEsquivel <lucianostivy@gmail.com>
2026-04-26 22:30:48 -0300

    22.30

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dietrich-unificado-V26.04.2026.22.11'; // Nueva versión unificada
+const CACHE_NAME = 'dietrich-unificado-V26.04.2026.22.30'; // Nueva versión unificada
 
 const assets = [
   // --- ARCHIVOS SIMULADOR (RAÍZ) ---
