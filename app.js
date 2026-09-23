bc178abe764cf8f2b8b1caf3d149ec5d62ac0a89
LucianoEsquivel <lucianostivy@gmail.com>
2026-09-23 11:53:58 -0300

    11.53

--- app.js
+++ app.js
@@ -762,7 +762,7 @@ const bancos = {
                         descripcion: "TASA FIJA, Fecha de vigencia: 17/09/2026 aplica para Dolphin mini GL y GS, ATTO 2 y Song PRO GL y GS, la cuota incluye intereses e IVA",
                         utilPara: "DOLPHIN MINI GL, GS, ATTO 2 y Song PRO GL y GS",
                         plazos: [
-                                    { m: 12, tna: "19,9%", ltv: 25000000, c: 92.90, g: 4}, 
+                                    { m: 12, tna: "19,9%", ltv: 25000000, c: 94.53, g: 4}, 
                                     { m: 18, tna: "19,9%", ltv: 20000000, c: 66.64, g: 4},
                                 ] 
                     },
